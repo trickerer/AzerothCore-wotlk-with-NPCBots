c7dd5af1f0d74c7ad8687f7c17ecb7058bba1ff0
Conan1996-2 <71103778+Conan1996-2@users.noreply.github.com>
2026-01-08 22:58:36 -0500

    Delete data/sql/custom/db_world/2026_01_08_creature_template_wander_nodes.sql

--- data/sql/custom/db_world/2026_01_08_creature_template_wander_nodes.sql
+++
@@ -1,2 +0,0 @@
---
-ALTER TABLE `creature_template_npcbot_wander_nodes` ADD `minwaittime` INT UNSIGNED NOT NULL DEFAULT '3000' AFTER `maxlevel`, ADD `maxwaittime` INT UNSIGNED NOT NULL DEFAULT '7000' AFTER `minwaittime`, ADD `proximity` FLOAT NOT NULL DEFAULT '0' AFTER `maxwaittime`;
