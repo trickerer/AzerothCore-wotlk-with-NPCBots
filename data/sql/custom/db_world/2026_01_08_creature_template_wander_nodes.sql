5e880a62eac3697b38f09dc0c78402a2ef822210
Conan1996-2 <71103778+Conan1996-2@users.noreply.github.com>
2026-01-08 22:26:16 -0500

    Update 2026_01_08_creature_template_wander_nodes.sql

--- data/sql/custom/db_world/2026_01_08_creature_template_wander_nodes.sql
+++ data/sql/custom/db_world/2026_01_08_creature_template_wander_nodes.sql
@@ -1,2 +1,2 @@
 --
-ALTER TABLE `creature_template_npcbot_wander_nodes` ADD `minwaittime` INT UNSIGNED NOT NULL DEFAULT '3000' AFTER `maxlevel`, ADD `maxwaittime` INT UNSIGNED NOT NULL DEFAULT '7000' AFTER `minwaittime`, ADD `proximity` FLOAT NOT NULL DEFAULT '0' AFTER `maxwaittiome`;
+ALTER TABLE `creature_template_npcbot_wander_nodes` ADD `minwaittime` INT UNSIGNED NOT NULL DEFAULT '3000' AFTER `maxlevel`, ADD `maxwaittime` INT UNSIGNED NOT NULL DEFAULT '7000' AFTER `minwaittime`, ADD `proximity` FLOAT NOT NULL DEFAULT '0' AFTER `maxwaittime`;
